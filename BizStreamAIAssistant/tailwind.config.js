4a0b61777d2768702ae184dcc919b65feacc3fe6
ylam <ylam@bizstream.com>
2025-05-14 15:54:29 -0400

    initial mvc setup, configured tailwindcss

---
+++ BizStreamAIAssistant/tailwind.config.js
@@ -0,0 +1,24 @@
+﻿///** @type {import('tailwindcss').Config} */
+//module.exports = {
+//    content: [
+//        './**/*.html', // Add all paths where your HTML or Razor files exist
+//        './**/*.razor',
+//        './**/*.cshtml',
+//    ],
+//    theme: {
+//        extend: {},
+//    },
+//    plugins: [],
+//}
+
+
+module.exports = {
+    content: [
+        './Views/**/*.cshtml',
+        './Pages/**/*.cshtml',  // if using Razor Pages too
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
